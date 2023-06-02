2a956979200019918c45bef701b8e5ba0c2f8928
pemma <emma.picard@ynov.com>
2023-06-02 17:07:01 +0200

    page login fini

---
+++ server.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func main() {
+	//Demarrage du Serveur
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", LoginPage)
+
+	fmt.Println("http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+
+}
+
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+func LoginPage(w http.ResponseWriter, r *http.Request) {
+
+	tmpl, _ := template.ParseFiles("./html/pages/loginpage.html", "./html/templates/header.html")
+
+	tmpl.Execute(w, "")
+}
