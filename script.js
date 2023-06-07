cda570f2ea21dc859177efee99ce7ab6b7a7ccb7
sleo <leo.sallet@ynov.com>
2023-06-07 14:10:30 +0200

    responsive

--- script.js
+++ script.js
@@ -1,35 +1,13 @@
-// Sélection des boutons
-const button1 = document.getElementById('button1');
-const button2 = document.getElementById('button2');
-const likedpostcontents = document.getElementById('likedposts')
-const ourpostcontents = document.getElementById('ourposts')
+// Sélectionne la balise <a> de la div "password"
+var link = document.querySelector('#passwordlink');
 
+// Sélectionne la popup box
+var popup = document.getElementById('popup');
 
-// Appliquer la classe active au premier bouton par défaut
-button1.classList.add('active');
+// Ajoute un gestionnaire d'événement de clic sur le lien
+link.addEventListener('click', function(event) {
+  event.preventDefault(); // Empêche le comportement par défaut du lien
 
-// Événements de clic sur les boutons
-button1.addEventListener('click', () => {
-    button1.classList.add('active');
-    button2.classList.remove('active');
-    likedpostcontents.style.display ='none';
-    ourpostcontents.style.display='flex'
+  // Affiche la popup box
+  popup.style.display = 'block';
 });
-
-button2.addEventListener('click', () => {
-    button1.classList.remove('active');
-    button2.classList.add('active');
-    
-
-    
-});
-
-
-button2.addEventListener('click', () => { 
-    likedpostcontents.style.display ='flex';
-    ourpostcontents.style.display='none';
-})
-
-button1.addEventListener('click', () => { 
-    ourpostcontents.style.display ='flex';
-})
