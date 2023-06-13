9d1c24598c30a6aea11c66788471cf389609ffc4
pemma <emma.picard@ynov.com>
2023-06-13 17:00:17 +0200

    responsive ok mais pas pour les popup

--- static/script/profilpage.js
+++ static/script/profilpage.js
@@ -1,4 +1,5 @@
-console.log("test");
+
+//ANIMATION POSTS
 const button1 = document.getElementById('button1');
 const button2 = document.getElementById('button2');
 const likedpostcontents = document.getElementById('likedposts')
@@ -31,7 +32,7 @@ button1.addEventListener('click', () => {
     ourpostcontents.style.display ='flex';
 })
 
-
+//POPUP CHANGE PASSWORD
 let link = document.querySelector('#passwordlink');
 let link2 = document.querySelector('#submit');
 let popup = document.querySelector('.overlay');
@@ -47,6 +48,55 @@ link2.addEventListener('click', function(event) {
     event.preventDefault(); 
     popup.style.display = 'none';
   });
+
+//POPUP VIEW FRIENDS
+let viewFriend = document.querySelector('#viewfriends');
+let popupFriend = document.querySelector('.overlayFriend');
+let closepopup = document.querySelector('#closepopup');
+
+viewFriend.addEventListener('click', function(event) {
+    event.preventDefault();  
+    popupFriend.style.display = 'block';
+  });
+  
+  
+closepopup.addEventListener('click', function(event) {
+      event.preventDefault(); 
+      popupFriend.style.display = 'none';
+    });
+
+//ANIMATION FRIENDS
+const friends = document.getElementById('button-friends');
+const block = document.getElementById('button-block');
+const blockeduser = document.getElementById('blocked')
+const followers = document.getElementById('followers')
+
+
+
+friends.classList.add('active');
+
+
+friends.addEventListener('click', () => {
+    friends.classList.add('active');
+    block.classList.remove('active');
+    blockeduser.style.display = 'none';
+    followers.style.display = 'flex'
+});
+
+block.addEventListener('click', () => {
+    friends.classList.remove('active');
+    block.classList.add('active');
+
+});
+
+block.addEventListener('click', () => { 
+    blockeduser.style.display ='flex';
+    followers.style.display='none';
+})
+
+friends.addEventListener('click', () => { 
+    followers.style.display ='flex';
+})
 
 
 let biographyElement = document.querySelector('.biography');
